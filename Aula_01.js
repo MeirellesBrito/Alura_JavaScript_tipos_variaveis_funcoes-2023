e6aaccb7d2892c2e89642fefa7dee1b7955d9b87
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-01-26 06:44:53 -0300

    first commit

---
+++ Aula_01.js
@@ -0,0 +1,23 @@
+//tipo Number
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const opracaoMatematica = primeiroNumero * segundoNumero;
+console.log(opracaoMatematica);
+
+
+//ponto flutuante
+const numeroPontoFLutuante = 3.3;
+const pontoFlutuante = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFLutuante;
+
+console.log(novaOperacao)
+
+
+//Nan -> not a Number (nao e um numero)
+const alura = "Alura";
+console.log(alura* primeiroNumero)
+
---
+++ Aula_02.js
@@ -0,0 +1,18 @@
+   //type-string 
+
+    const text = "Ola, mundo!";
+    const text2 = 'Ola, mundo!';
+    const senha = "senhaSuperSegura!";
+    const stringDeNumeros = "34567";
+    
+    const citacao = "Meu nome e ";
+    const meuNome = "Meirelles";
+    console.log(citacao)
+    
+    //Concatenacao (+)
+    console.log(citacao+meuNome)
+    
+    
+    //Template string Ou template literal
+   
+    
