c04e5564f03f1e7a94921be91765efa0c559cd02
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-01-26 06:49:26 -0300

    first commit

--- Aula_03.js
+++ Aula_03.js
@@ -1,17 +1,5 @@
-    //type-string 
+//boolean
+const primeiroNumero = 5;
+const segundoNumeor = 10;
 
-    const text = "Ola, mundo!";
-    const text2 = 'Ola, mundo!';
-    const senha = "senhaSuperSegura!";
-    const stringDeNumeros = "34567";
-    
-    const citacao = "Meu nome e ";
-    const meuNome = "Meirelles";
-    console.log(citacao)
-    
-    //Concatenacao (+)
-    console.log(citacao+meuNome)
-    
-    
-    //Template string Ou template literal
-    
+console.log(primeiroNumero === segundoNumeor);
