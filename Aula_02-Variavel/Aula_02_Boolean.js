d46c9b97de749245ec2e84464f74ed21b8b7decb
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-01-27 02:19:14 -0300

    Aula 2

---
+++ Aula_02-Variavel/Aula_02_Boolean.js
@@ -0,0 +1,28 @@
+//bolean
+// const usuarioLogado = true;
+// const contapaga = false;
+
+// //true ou false
+
+// //0 => false
+// //1 => true
+// // null => vazil ou nada
+// // undefined => false
+
+// console.log(0 == false)
+// console.log("" == false)
+// console.log("1" == true)
+
+// ________________________________________________
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let text = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
---
+++ Aula_02-Variavel/Aula_03_Tipos.js
@@ -0,0 +1,15 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita
+const numero = 456;
+const numeroString = "456";
+
+// console.log(numero + numeroString)
+
+
+
+// converso explicita
+//Number()
+//String
+console.log(numero + Number(numeroString))
